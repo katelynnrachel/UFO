ee195071620307b67955ffec5c882357875aac41
katelynnrachel <katelynn0721@gmail.com>
2022-12-09 06:18:50 -0800

    finalizing app.py

--- static/js/app.js
+++ static/js/app.js
@@ -24,24 +24,22 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-let filters = {};
+var filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    let changedElement = d3.select(this);
+  let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-    let elementValue = changedElement.property("value");
-    console.log(elementValue);
+  let elementValue = changedElement.property("value");
     // 4c. Save the id of the filter that was changed as a variable.
-    let filterId = changedElement.attr("id");
-  console.log(filterId);
+  let filterId = changedElement.attr("id");
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-    if (elementRevised){
-      filters[filterId] = elementRevised;
+    if (elementValue){
+      filters[filterId] = elementValue;
     }
     else {
       delete filters[filterId];
